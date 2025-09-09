c7d09bcb7e631515526c112bc0854b231ccaea3a
hemantssharma <113016398+hemantssharma@users.noreply.github.com>
2025-09-09 17:57:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.6.0"
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-aft-bootstrap"
+    key            = "aft-bootstrap/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "my-terraform-locks"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "aft" {
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+
+  ct_management_account_id  = "767397915550"
+  aft_management_account_id = "383975042464"
+  ct_home_region            = "us-east-1"
+
+  vcs_provider                                = "github"
+  account_request_repo_name                   = "hemantssharma/aft-account-request"
+  account_request_repo_branch                 = "main"
+  global_customizations_repo_name             = "hemantssharma/aft-global-customizations"
+  account_customizations_repo_name            = "hemantssharma/aft-account-customizations"
+  account_provisioning_customizations_repo_name = "hemantssharma/aft-provisioning-customizations"
+}
