88ad0dd422a85b6054c82e9acab4c6b6445a8775
hemantssharma <113016398+hemantssharma@users.noreply.github.com>
2025-09-15 23:57:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ terraform {
   required_version = ">= 1.6.0"
  
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-aftbootstrap"
+    bucket         = "my-terraform-state-bucket--aftbootstrap"
     key            = "aft-bootstrap/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     dynamodb_table = "my-terraform-locks"
   }
 }
